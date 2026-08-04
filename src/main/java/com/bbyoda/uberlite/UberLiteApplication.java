ca7887eaf461324a8f9ce1777856d001e2bafd08
bbyoda <h2trungvuong@gmail.com>
2026-08-04 23:43:23 +0200

    Initial commit

---
+++ src/main/java/com/bbyoda/uberlite/UberLiteApplication.java
@@ -0,0 +1,13 @@
+package com.bbyoda.uberlite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberLiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UberLiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bbyoda/uberlite/UberLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbyoda.uberlite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberLiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
