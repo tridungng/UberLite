c0cd7a80f1f405e57d8889af39505c28e0ea7bd5
bbyoda <h2trungvuong@gmail.com>
2026-08-07 20:51:52 +0200

    Add route-service and time-estimation-service modules with initial POM configuration and application class

---
+++ time-estimation-service/src/main/java/com/uberlite/timeestimation/TimeEstimationApplication.java
@@ -0,0 +1,11 @@
+package com.uberlite.timeestimation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeEstimationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TimeEstimationApplication.class, args);
+    }
+}
